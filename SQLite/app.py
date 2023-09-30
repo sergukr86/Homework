7ceac4cb381d3cab99076f8995150d8bf6078331
serg <sergukr86@gmail.com>
2023-09-30 17:39:28 +0200

    Add views with connections to music.db

---
+++ SQLite/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello!"
+
+
+@app.route("/tracks")
+def tracks():
+    con = sqlite3.connect("music.db")
+    cur = con.cursor()
+    cur.execute("SELECT COUNT(*) FROM tracks")
+    cnt_lines = cur.fetchone()
+    con.close()
+    return render_template("music.html", cnt_lines=cnt_lines)
+
+
+@app.route("/customers")
+def customers():
+    con = sqlite3.connect("music.db")
+    cur = con.cursor()
+    cur.execute("SELECT DISTINCT first_name FROM customers")
+    f_name = cur.fetchall()
+    con.close()
+    return render_template("customers.html", f_name=f_name)
+
+
+@app.route("/tracks-sec")
+def duration():
+    con = sqlite3.connect("music.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM tracks")
+    tracks_info = cur.fetchall()
+    con.close()
+    return render_template("tracks.html", tracks_info=tracks_info)
+
+
+if __name__ == "__main-__":
+    app.run(debug=True)
--- SQLite/db.py
+++ SQLite/db.py
@@ -7,7 +7,7 @@ def create_cust(num):
     fake = Faker()
     data = []
     for i in range(0,num):
-        data.append((fake.name(), fake.address(), int(round(random.random(), 2) * 100)))
+        data.append((fake.first_name(), fake.last_name(), fake.address(), int(round(random.random(), 2) * 100)))
     return data
 
 
@@ -25,9 +25,10 @@ cur = con.cursor()
 cur.execute("DROP TABLE IF EXISTS customers;")
 cur.execute('''CREATE TABLE customers (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name VARCHAR NOT NULL,
+            first_name VARCHAR NOT NULL,
+            last_name VARCHAR NOT NULL,
             address VARCHAR NOT NULL,
-            age VARCHAR(2)
+            age VARCHAR(2) NOT NULL
             )''')
 
 cur.execute("DROP TABLE IF EXISTS tracks;")
@@ -35,11 +36,11 @@ cur.execute('''CREATE TABLE tracks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             artist VARCHAR NOT NULL,
             release_date DATE NOT NULL,
-            lasting_time VARCHAR(3)
+            lasting_time INTEGER(3)
             )''')
 
 customer_data = create_cust(100)
-cur.executemany("INSERT INTO customers (name, address, age) VALUES (?, ?, ?)", customer_data)
+cur.executemany("INSERT INTO customers (first_name, last_name, address, age) VALUES (?, ?, ?, ?)", customer_data)
 
 track_data = create_track(100)
 cur.executemany("INSERT INTO tracks (artist, release_date, lasting_time) VALUES (?, ?, ?)", track_data)
